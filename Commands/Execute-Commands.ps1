c985e0944b52e9bcb29f3489159791887b991888
Johan Andersson <johan-spam@andersson.net>
2012-08-28 17:21:36 +0200

    Added a simple command executution script to be used with our samples. Requires cURL.

---
+++ Commands/Execute-Commands.ps1
@@ -0,0 +1,89 @@
+﻿<#
+.SYNOPSIS
+	Executes a command batch from a file and returns its result
+.DESCRIPTION
+	This script uses cURL to communicate with the RemoteX web services.
+	It can be obtained from http://curl.haxx.se/download.html
+.PARAMETER Path
+	The path to the XML file with the commands to be executed
+.PARAMETER ServiceUri
+	The url to the web service - must end with /api
+.PARAMETER Username
+	The username to authenticate as
+.PARAMETER Password
+	The password to use for authentication
+#>
+param( 
+	[parameter(mandatory=$true)]
+	$Path,
+	[parameter(mandatory=$true)]
+	[ValidateScript({ $_ -imatch "^https?://.+/api/?$" })]
+	$ServiceUri,
+	[parameter(mandatory=$true)]
+	$Username,
+	[parameter(mandatory=$true)]
+	$Password
+)
+
+if( !( Get-Command curl ) ) {
+	Write-Error "Could not find curl command in the path"
+	exit 1
+}
+
+$file = gi -ErrorAction SilentlyContinue $Path
+if( !$file ) {
+	Write-Error ("Could not find the file '{0}'" -f $Path)
+	exit 2
+}
+
+if( $file.Extension -ne ".xml" ) {
+	Write-Error ("Can only use XML files '{0}'" -f $Path)
+	exit 3
+}
+$outputFile = "{0}\{1}_output.xml" -f $file.Directory, $file.BaseName
+$traceFile = "{0}\{1}_trace.txt" -f $file.Directory, $file.BaseName
+
+$url = "{0}/commands" -f $ServiceUri.TrimEnd("/")
+$cred = "{0}:{1}" -f $Username, $Password
+curl -s --trace $traceFile -o $outputFile -u $cred -X POST -T $file.FullName -H 'Content-Type: application/xml' -H 'Accept: application/xml' $url
+try {
+	$xml = ([xml](gc $outputFile)).CommandBatchResponse
+} catch {
+	Write-Error ("Error in XML: {0}" -f $_ )
+	exit 4
+}
+
+if(!$?) {
+	Write-Error ("Curl failed with error {0}" -f $LASTEXITCODE)
+	exit 5
+}
+
+Write-Host ("Response written to {0}" -f $outputFile)
+Write-Host ("cURL trace output written to {0}" -f $traceFile)
+
+if($xml.HasErrors -eq "true") {
+	Write-Host -ForegroundColor Red "Command execution failed!"
+} else {
+	Write-Host "Successfully executed commands:"
+}
+
+$xml.CommandResponse | %{
+	Write-Host -NoNewline "Command: "
+	Write-Host -ForegroundColor Yellow $_.Command.Name
+	if( $_.HasErrors ) {
+		Write-Host -NoNewline "  Error: "
+		Write-Host -ForegroundColor Red $_.ErrorMessage
+	}
+	$_.Command.Parameter | %{
+		Write-Host -NoNewline "   Parameter: "
+		Write-Host -ForegroundColor Yellow $_.Name
+		Write-Host -NoNewline "       Value: "
+		Write-Host -ForegroundColor Yellow $_.Value
+	}
+	Write-Host "Affected items: "
+	$_.AffectedItems.Item | %{
+		Write-Host -ForegroundColor Yellow ("   {0} (Revision: {1})" -f $_.Href, $_.Revision)
+	}
+	Write-Host ""
+}
+	
