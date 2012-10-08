33b4e034485353fed4382b7d5b27c4b21f8c765a
Johan Andersson <johan-spam@andersson.net>
2012-10-08 14:15:16 +0200

    Added script to fetch available commands from the API

---
+++ Commands/Get-AvailableCommands.ps1
@@ -0,0 +1,17 @@
+﻿param( 
+	[parameter(mandatory=$true)]
+	[ValidateScript({ $_ -imatch "^https?://.+/api/?$" })]
+	$ServiceUri,
+	[parameter(mandatory=$true)]
+	$Username,
+	[parameter(mandatory=$true)]
+	$Password
+)
+
+function curlGet( $ServiceUri, $Username, $Password, $href ) {
+	$cred = "{0}:{1}" -f $Username, $Password
+	[xml](curl -s -u $cred -H 'Accept: application/xml' $ServiceUri/$href)
+}
+
+$availableCommands = curlGet $ServiceUri $Username $Password "commands"
+$availableCommands.AvailableCommands.Commands.Command
